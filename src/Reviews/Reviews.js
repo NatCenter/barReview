6fb5d21527a73b5955e4b3b8c7b661fead8f6890
Nathan Center <nathanjcenter@gmail.com>
2022-01-31 15:14:40 -0600

    rewrote code so that it can render the reviews for each bar

--- src/Reviews/Reviews.js
+++ src/Reviews/Reviews.js
@@ -2,58 +2,45 @@ import { Link } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { useEffect, useState } from "react/cjs/react.development";
 import { useParams } from "react-router-dom";
-import "./Reviews.css"
-
+import "./Reviews.css";
 
 export const Reviews = () => {
-    const [bar,changeBar]=useState({})
-    const [images,changeImages]=useState([])
-    
-    const {barId}=useParams()
-    const {imageURL}=useParams()
-    
-    
-    useEffect(
-        ()=>{
-            return fetch(`http://localhost:8088/bars/${barId}`)
-            .then(barData=>barData.json())
-            .then((bar)=>{
-                changeBar(bar)
-                
-                return fetch(`http://localhost:8088/imageBars?barId=${bar.id}`)
-                .then(renderId=>renderId.json())
-                .then((data)=>{
-                    changeImages(data)
-                })
-            })
-        },[barId]
-    
-    )
-    
+  const [bar, changeBar] = useState({});
+  
 
-    console.log(images)
-    return (
-        <>
-       
+  const { barId } = useParams();
 
-        <h1>{bar.barName}</h1>
-            
-          {
-               images.map((image)=>{
-                  return(
-                    <>
-                   <img src={image.imageURL1}/>
-                   <img src={image.imageURL2}/>
-                   <img src={image.imageURL3}/>
-                   <img src={image.imageURL4}/>
-                   </>
-                   
-                   )
-               })
-          }
-           
-
-        </>
-
-    )
+  useEffect(() => {
+    return fetch(`http://localhost:8088/bars/${barId}?_embed=imageBars&_embed=reviews`)
+      .then((data) => data.json())
+      .then((barData) => {
+        changeBar(barData);
+      });
+  }, []);
+  
+  return (
+    <>
+      <h1>{bar.barName}</h1>
+      <p>{bar.address}</p>
+      {bar.imageBars?.map((image) => {
+        return (
+          <>
+            <img src={image.imageURL1} />
+            <img src={image.imageURL2} />
+            <img src={image.imageURL3} />
+            <img src={image.imageURL4} />
+          </>
+        );
+      })}
+      <p>
+        {bar.reviews?.map((review) => {
+          return (
+            <>
+              <p>{review.reviewDes}</p>
+            </>
+          );
+        })}
+      </p>
+    </>
+  );
 };
