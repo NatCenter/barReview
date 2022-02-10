d7f864a2871abfd4e09edefe495bc7243b26a38a
Nathan Center <nathanjcenter@gmail.com>
2022-02-10 10:39:10 -0600

    select works now!

--- src/Reviews/newReview.js
+++ src/Reviews/newReview.js
@@ -3,11 +3,22 @@ import { useEffect, useState } from "react/cjs/react.development";
 import ReactStars from "react-rating-stars-component";
 import React from "react";
 import { render } from "react-dom";
-import Button from '@mui/material/Button';
-
+import Button from "@mui/material/Button";
 import "./newReviewCss.css";
 
 export const NewReviews = () => {
+  // make a fetch that gets all the bars in the database
+  const [barList, setBars] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8088/bars")
+      .then((res) => res.json())
+      .then((barArray) => {
+        setBars(barArray);
+      });
+  }, []);
+
+  console.log(barList);
   const [newReview, updateReview] = useState({
     description: "",
     barId: "",
@@ -42,56 +53,61 @@ export const NewReviews = () => {
   };
   return (
     <>
-    <div className="newReviewForm">
-      <h1>Place your review here!</h1>
-      <label>Review:</label>
-      <textarea
-        type="text"
-        onChange={(evt) => {
-          const copy = { ...newReview };
-          copy.description = evt.target.value;
-          updateReview(copy);
-        }}
-      />
-      <br></br>
-      <select
-        onChange={(evt) => {
-          const copy = { ...newReview };
-          copy.barId = evt.target.value;
-          updateReview(copy);
-        }}
-      >
-        <option value={1}>Bar Sovereign</option>
-        <option value={2}>Bar Louie - Nashville</option>
-        <option value={3}>Gertie's Whiskey Bar: Nashville, Tennessee</option>
-        <option value={4}>Hops and Crafts</option>
-      </select>
-      <br></br>
-      <label>Upload image by copying and pasting the url </label>
-      
-      <input
-        onChange={(evt) => {
-          const copy = { ...newReview };
-          copy.userImageReview = evt.target.value;
-          updateReview(copy);
-        }}
-      />
-      <br></br>
-      <label>Star(s)</label>
-      <ReactStars
-        count={5}
-        onChange={(evt) => {
-          const copy = { ...newReview };
-          copy.star = evt;
-          updateReview(copy);
-        }}
-        size={24}
-        activeColor="#ffd700"
-      />
-      <br></br>
-      <Button className="sumbitReview"  variant="contained"  type="submit" onClick={sumbitReview}>
-        Submit
-      </Button>
+      <div className="newReviewForm">
+        <h1>Place your review here!</h1>
+        <label>Review:</label>
+        <textarea
+          type="text"
+          onChange={(evt) => {
+            const copy = { ...newReview };
+            copy.description = evt.target.value;
+            updateReview(copy);
+          }}
+        />
+        <br></br>
+        <select
+          onChange={(evt) => {
+            const copy = { ...newReview };
+            copy.barId = parseInt(evt.target.value);
+            updateReview(copy);
+          }}
+        >
+          {barList.map((bar) => {
+            return (<option value={bar.id}>{bar.barName}</option>);
+          })}
+        </select>
+        <br></br>
+
+        <label>Upload image by copying and pasting the url </label>
+
+        <input
+          onChange={(evt) => {
+            const copy = { ...newReview };
+            copy.userImageReview = evt.target.value;
+            updateReview(copy);
+          }}
+        />
+        <br></br>
+        <label>Star(s)</label>
+        <ReactStars
+          count={5}
+          onChange={(evt) => {
+            const copy = { ...newReview };
+            copy.star = evt;
+            updateReview(copy);
+          }}
+          size={24}
+          activeColor="#ffd700"
+        />
+        <br></br>
+        <Button
+          className="sumbitReview"
+          variant="contained"
+          type="submit"
+          onClick={sumbitReview}
+        >
+          Submit
+        </Button>
       </div>
     </>
   );
